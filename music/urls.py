b14142d47ff37450d4b15ca09aea72cd4c37dfcb
niru696 <nkchoudhary696@gmail.com>
2017-10-09 23:36:30 +0530

    initial commit

---
+++ music/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'music'
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+
+    url(r'^(?P<album_id>[0-9]+)/favorite$', views.favorite, name='fevorite'),
+]
